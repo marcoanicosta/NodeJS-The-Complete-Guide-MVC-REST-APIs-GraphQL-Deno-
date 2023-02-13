8f1db465ff7db1ed2ace6d653efe35d2de899817
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-02-13 11:57:24 +0000

    js-rest+spread simple

--- play.js
+++ play.js
@@ -13,5 +13,13 @@ const hobbies = ["Sports", "Cooking"];
 
 // console.log(hobbies.map(hobby =>  'Hobby: ' + hobby));
 // console.log(hobbies); 
-hobbies.push('Programming');
-console.log(hobbies);
+// hobbies.push('Programming');
+// console.log(hobbies);
+const copiedArray = [...hobbies]
+console.log(copiedArray);
+
+const toArray = (arg1, arg2, arg3) => {
+    return [arg1, arg2, arg3]
+};
+
+console.log(toArray(1,2,3));
