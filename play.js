d9644198ebf46100d100f78907528000d0d164e1
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-02-13 18:40:00 +0000

    js-async simple

--- play.js
+++ play.js
@@ -1,35 +1,17 @@
-const person = {
-    name: 'Max',
-    age: 29,
-    greet()  {
-        console.log('Hi, I am ' + this.name);
-    }
-};
+const fetchData = () => {
+    const promise = new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });  
+    return promise;
+ };
 
-const printName = ({name}) => {
-    console.log(name);
-}
 
-printName(person);
-const {name, age} = person;
-console.log(name, age);
 
-const hobbies = ["Sports", "Cooking"];
-const [hobby1, hobby2] = hobbies
-console.log(hobby1, hobby2);
-// // for (let hobby of hobbies) {
-// //     //console.log(hobby);
-// // }
-
-// // console.log(hobbies.map(hobby =>  'Hobby: ' + hobby));
-// // console.log(hobbies); 
-// // hobbies.push('Programming');
-// // console.log(hobbies);
-// const copiedArray = [...hobbies]
-// console.log(copiedArray);
-
-// const toArray = (arg1, arg2, arg3) => {
-//     return [arg1, arg2, arg3]
-// };
-
-// console.log(toArray(1,2,3));
+setTimeout(() => {
+    console.log('Timer is done!');
+    fetchData().then(text => {
+        console.log(text);
+    });
+}, 2000);
