75a37f845b81d8ed8bf11077cd4541314681b11c
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-02-13 18:26:40 +0000

    js-destructuring simple

--- play.js
+++ play.js
@@ -6,20 +6,30 @@ const person = {
     }
 };
 
+const printName = ({name}) => {
+    console.log(name);
+}
+
+printName(person);
+const {name, age} = person;
+console.log(name, age);
+
 const hobbies = ["Sports", "Cooking"];
-// for (let hobby of hobbies) {
-//     //console.log(hobby);
-// }
+const [hobby1, hobby2] = hobbies
+console.log(hobby1, hobby2);
+// // for (let hobby of hobbies) {
+// //     //console.log(hobby);
+// // }
 
-// console.log(hobbies.map(hobby =>  'Hobby: ' + hobby));
-// console.log(hobbies); 
-// hobbies.push('Programming');
-// console.log(hobbies);
-const copiedArray = [...hobbies]
-console.log(copiedArray);
+// // console.log(hobbies.map(hobby =>  'Hobby: ' + hobby));
+// // console.log(hobbies); 
+// // hobbies.push('Programming');
+// // console.log(hobbies);
+// const copiedArray = [...hobbies]
+// console.log(copiedArray);
 
-const toArray = (arg1, arg2, arg3) => {
-    return [arg1, arg2, arg3]
-};
+// const toArray = (arg1, arg2, arg3) => {
+//     return [arg1, arg2, arg3]
+// };
 
-console.log(toArray(1,2,3));
+// console.log(toArray(1,2,3));
