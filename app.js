420d4adf5f5d3ef81ab80d2720dadb78611f3fe5
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-02-15 22:33:09 +0000

    server res

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First HTML</title></head>');
+    res.write('<body>Hello from the Node.js Server 📟...</body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
