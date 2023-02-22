c44debdfcb80d18b811d0efc2b92f4d8bbc0bddf
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2023-02-22 20:46:28 +0000

    understanding event driven code execution

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ const server = http.createServer((req, res) => {
             const parseBody = Buffer.concat(body).toString();
             const message = parseBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
 
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
